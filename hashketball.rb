4a9bae824ebec79eddaba481b0fdc07b40809423
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 20:17:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,3 +217,17 @@ def player_stats(players_name)
   end
   new_hash
 end
+
+def big_shoe_rebounds
+  big_shoes_guy = 0
+  rebounds = 0
+    game_hash.each do | team, team_details_hash|
+      team_details_hash[:players].each do | stats |
+        if stats[:shoe] > big_shoes_guy
+          big_shoes_guy = stats[:shoe]
+          rebounds = stats[:rebounds]
+        end
+      end
+    end
+  rebounds
+end
