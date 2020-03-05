95017cbcbe1fb3141a2961fed167a2317e745685
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 20:14:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -205,8 +205,15 @@ def player_stats(players_name)
   game_hash.each do |place, team|
     team.each do |attributes, data|
       if attributes == :players
-        
+        data.each do |player|
+          if player[:player_name] == players_name
+            new_hash = player.delete_if do |k, v|
+              k == :player_name
+            end
+          end
+        end
       end
     end
   end
+  new_hash
 end
