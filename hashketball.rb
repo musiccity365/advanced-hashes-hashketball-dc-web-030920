b070427d4b4fd8e1babeea15455d8f2bdd4eaa0b
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:58:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -44,7 +44,16 @@ def game_hash
         :slam_dunks => 0
       },
       { :player_name => "Kemba Walker",
-        
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12
+      },
+      { :player_name => 
         
       }
       ]
