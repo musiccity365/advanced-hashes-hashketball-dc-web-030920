265fcb978883837ef611f9df979ae3bad8b1f3d0
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 19:27:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,10 +115,3 @@ def game_hash
           }
 end
 
-def num_points_scored(players_name)
-  #return num of points for player passed in
-  game_hash.each do |place, team|
-    team.each do |attribute, data|
-      binding.pry
-  end
-end
