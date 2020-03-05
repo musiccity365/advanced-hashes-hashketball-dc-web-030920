b3e636116de133f85efbbd6d5f4c2b25ec6af8ef
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 20:03:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -166,6 +166,7 @@ end
 #  new_arr
 #end
 
+=begin
 def player_numbers(team_name)
   #returns array of jersey numbers for that team
   game_hash.map do |place, team|
@@ -182,7 +183,7 @@ def player_numbers(team_name)
   nums
 #end
 
-= begin
+
 def player_numbers(team_name)
    game_hash.map do |place, team|
      if team[:team_name] == team_name
