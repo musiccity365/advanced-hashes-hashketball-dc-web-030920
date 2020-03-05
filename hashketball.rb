fca46e0dce5dc73000c7839ee0aa1ed6f39f0dcc
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 19:34:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,5 +131,15 @@ def num_points_scored(players_name)
 end
 
 def shoe_size(players_name)
-  
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe_size]
+          end
+        end
+      end
+    end
+  end
 end
